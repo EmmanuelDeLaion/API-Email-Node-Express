eab5c0e10f386edade6749ad2cd8225d52f2b458
Mono <Mono@DESKTOP-BR5IUL7>
2021-02-20 21:10:39 -0600

    make it better

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const configMensaje = require('./configMensaje');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 app.post('/formulario', (req, res) => {
     configMensaje(req.body);
     res.status(200).send();
-})
-app.listen(3000, () => {
-    console.log('Servidor corriendo')
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
 });
 
